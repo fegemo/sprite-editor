400f5e3593b10b8708bd07484e143c5844727b32
Flávio Coutinho <fegemo@gmail.com>
2023-02-25 12:33:37 -0300

    Adds a version in the path of models

--- js/functional-util.js
+++ js/functional-util.js
@@ -9,6 +9,17 @@ export function* range(start, end, step = 1) {
   }
 }
 
+export function* zipDistinct(items1, items2=items1) {
+  for (let item1 of items1) {
+    for (let item2 of items2) {
+      if (item1 === item2) {
+        continue
+      }
+      yield [item1, item2]
+    }
+  }
+}
+
 export function callOrReturn(object, property, defaultValue = null, params = []) {
   const fnOrvalue = object[property]
   if (fnOrvalue) {
--- js/generators/config.js
+++ js/generators/config.js
@@ -1,3 +1,5 @@
+import { zipDistinct } from '../functional-util.js'
+
 export const DOMAINS = {
   front: 'front',
   left: 'left',
@@ -14,89 +16,52 @@ const modelUrlPrefix = localOrRemoteModels === 'remote' ? 'https://fegemo.github
 export const config = {
   pix2pix: {
     inputs: ['sourceImage'],
-    checkpoints: [
-      {
-        file: modelUrlPrefix + 'pixel-sides-models/pix2pix/front-to-left/model.json',
-        source: DOMAINS.front,
-        target: DOMAINS.left
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/front-to-back/model.json',
-        source: DOMAINS.front,
-        target: DOMAINS.back
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/front-to-right/model.json',
-        source: DOMAINS.front,
-        target: DOMAINS.right
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/back-to-left/model.json',
-        source: DOMAINS.back,
-        target: DOMAINS.left
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/back-to-front/model.json',
-        source: DOMAINS.back,
-        target: DOMAINS.front
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/back-to-right/model.json',
-        source: DOMAINS.back,
-        target: DOMAINS.right
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/right-to-left/model.json',
-        source: DOMAINS.right,
-        target: DOMAINS.left
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/right-to-front/model.json',
-        source: DOMAINS.right,
-        target: DOMAINS.front
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/right-to-back/model.json',
-        source: DOMAINS.right,
-        target: DOMAINS.back
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/left-to-right/model.json',
-        source: DOMAINS.left,
-        target: DOMAINS.right
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/left-to-front/model.json',
-        source: DOMAINS.left,
-        target: DOMAINS.front
-      },
-      {
-        file:  modelUrlPrefix + 'pixel-sides-models/pix2pix/left-to-back/model.json',
-        source: DOMAINS.left,
-        target: DOMAINS.back
-      },
-    ],
-    endpoint: '/api/pix2pix/{source}/2/{target}'
+    name: 'pix2pix',
+    version: 'all,230224',
+    get checkpoints() {
+      return Array.from(zipDistinct([DOMAINS.front, DOMAINS.right, DOMAINS.back, DOMAINS.left]))
+        .map(([source, target]) => ({
+          source,
+          target,
+          file: `${modelUrlPrefix}pixel-sides-models/${this.name}/${this.version}/${source}-to-${target}/model.json`
+        })
+        )
+    },
+    get endpoint() {
+      return `/api/${this.name}/${this.version}/{source}/2/{target}`
+    }
   },
   stargan: {
     inputs: ['sourceImage', 'targetDomain'],
+    name: 'stargan',
+    version: '',
     checkpoints: [
       {
-        file:  modelUrlPrefix + 'pixel-sides-models/stargan/model.json',
+        get file() {
+          return `${modelUrlPrefix}pixel-sides-models/${this.name}/${this.version}/model.json`
+        },
         source: DOMAINS.any,
         target: DOMAINS.any
       }
     ],
-    endpoint: '/api/stargan/2/{target}'
+    get endpoint() {
+      return `/api/${this.name}/${this.version}/2/{target}`
+    }
   },
   collagan: {
+    name: 'collagan',
+    version: '',
     checkpoints: [
       {
-        file:  modelUrlPrefix + 'pixel-sides-models/collagan/model.json',
+        get file() {
+          return `${modelUrlPrefix}pixel-sides-models/${this.name}/${this.version}/model.json`
+        },
         source: DOMAINS.many,
         target: DOMAINS.any
-      }      
+      }
     ],
-    endpoint: '/api/collagan/2/{target}'
+    get endpoint() {
+      return `/api/${this.name}/${this.version}/2/{target}`
+    }
   }
 }
